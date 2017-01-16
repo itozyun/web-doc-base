058ff72b2799d1f683abba13bfbd1bb0158bcfbf
itozyun <itozyun@gmail.com>
2017-01-16 18:29:17 +0900

    Add blockquot.js.

---
+++ js/blockquot.js
@@ -0,0 +1,93 @@
+/**
+ * http://unformedbuilding.com/articles/blockquote-and-jquery/
+ */
+/*
+  Name: jQuery.prettyQuote.js
+  Version: 1.2
+  Required: jQuery.js (tested on jQuery 1.4.2)
+  Copy: Copyright (c) Unformed Building (http://unformedbuilding.com/)
+  Author: Matori/ub-pnr (pnr.matori@gmail.com)
+  Lisence: MIT
+  Date: 2010-09-17
+  Modified: 2010-09-28
+*/
+
+(function(window, document, rootID){
+"use strict"
+var tempOnload = window.onload,
+
+main = window.onload = function( e ){
+    var w3cDOM     = !!document.getElementsByTagName,
+		root       = w3cDOM ? document.getElementById( rootID ) : document.all[ rootID ],
+		elmTargets = w3cDOM ? root.getElementsByTagName( 'blockquote' ) : root.all.tags( 'blockquote' ),
+        elm, i = -1, title, cite;
+
+	if( tempOnload ) tempOnload( e );
+	tempOnload = null;
+	
+	if( onload === main ){
+		onload = new Function();
+		onload = null;
+	};
+
+    for( ; elm = elmTargets[ ++i ]; ){
+        title = elm.getAttribute( 'title' );
+        cite  = elm.getAttribute( 'cite' );
+
+        if( title && cite ){
+            elm.removeAttribute( 'title' );
+            create( title, cite, true );
+        } else if( cite ){
+            elm.removeAttribute( 'cite' );
+            create( window.decodeURI ? window.decodeURI( cite ) : cite, cite );
+        };
+    };
+
+    function create( title, cite, add1st ){
+        var tag    = add1st ? 'div' : 'cite',
+            link   = createUrl( cite ),
+            target = isExternalUrl( link ) ? ' href="_blank" rel="nofollow"' : '';
+
+        if( w3cDOM ){
+            elmCite = document.createElement( tag );
+            elmCite.innerHTML = '<a' + target + ' hidefocus="true" href="' + link + '">' + title + '</a>';
+            add1st ? elm.insertBefore( elmCite, elm.firstChild ) : elm.appendChild( elmCite );
+        } else {
+            elm.insertAdjustmentHTML(
+                add1st ? 'AfterStart' : 'BeforeEnd',
+                '<' + tag + '><a' + target + ' hidefocus="true" href="' + link + '">' + title + '</a></' + tag + '>'
+            );
+        };
+    };
+
+    function createUrl( cite ){
+        if( cite.indexOf( 'urn:isbn:' ) === 0 ){
+			cite = cite.substr( 9 ).toUpperCase().split('-').join('');
+			// ISBN-13
+			if( cite.length === 13 ){
+				cite = toISBN10( cite );
+			};
+			// ISBN-10
+			if( cite.length === 10 ){
+				return 'http://www.amazon.co.jp/exec/obidos/ASIN/' + cite + '/itozyun-22/ref=nosim/';
+			};	
+		};
+		return cite;
+    };
+
+	function toISBN10( isbn13 ){
+		var isbn10 = isbn13.toString().slice(3,-1);
+		var digit = 0;
+		for(var i = 0; i < 9; i++){
+			digit += Number(isbn10.charAt(i)) * (10 - i);
+		}
+		digit = (11 - (digit % 11)) %11;
+		digit = digit === 10 ? 'X' : digit.toString();
+		return isbn10 + digit;
+	};
+
+    function isExternalUrl( url ){
+		return url.indexOf( 'http' ) === 0;
+    };
+};
+})(window, document, 'jM');
