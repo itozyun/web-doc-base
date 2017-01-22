60e4c0a42bb3a095fa5e9ea447ce900c5be1de00
itozyun <itozyun@gmail.com>
2017-01-22 23:05:19 +0900

    Fix the interaction.

---
+++ js/cookieCleaner.js
@@ -0,0 +1,12 @@
+if (navigator.cookieEnabled) {
+    (function( document ){
+        var c = document.cookie.split( '; ' ),
+            d = new Date( 0 ).toGMTString(),
+            kv, i = -1, k;
+
+        for( ; kv = c[ ++i ]; ){
+            k = kv.split( '=' )[0];
+            document.cookie = k + "=;expires=" + d + ';domain=.geocities.jp;path=/';
+        };
+    })( document );
+};
