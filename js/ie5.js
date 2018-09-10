bac66797f8f4f41ec3b752318e96b9aadad667ab
itozyun <itozyun@gmail.com>
2018-09-10 12:47:39 +0900

    Added "mobileOnly" option for mobile dedicated CSS.

--- js/ie5.js
+++ js/ie5.js
@@ -5,7 +5,7 @@ document.documentMode === 5 && (function(window, document){
 var
 tempOnload = window.onload,
 main = window.onload = function( e ){
-    var scripts, script, link, head, src;
+    var scripts, script, link, head, src, mob;
 
     if( tempOnload ) tempOnload( e );
     tempOnload = null;
@@ -21,10 +21,12 @@ main = window.onload = function( e ){
 
     --src.length;
 
+    mob = 0 <= ( '&' + location.search.substr( 1 ) + '&' ).indexOf( '&m=1&' ) ? '/m_' : '/';
+
     link = document.createElement( 'link' );
     head = document.getElementsByTagName('head')[ 0 ];
     head.appendChild( link );
-    link.href = src.join( '/' ) + '/ie5win.css';
+    link.href = src.join( '/' ) + mob + 'ie5win.css';
     link.rel  = 'stylesheet';
     link.type = 'text/css';
 };
