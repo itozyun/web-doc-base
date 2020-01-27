fdfaaade5da002f643636e4cbd7027da8bccfac3
itozyun <itozyun@gmail.com>
2020-01-27 20:26:00 +0900

    Fixed an issue that failed to determine the validity of the content property in Firefox 72, IE8~9, Edge.

--- js/6_CanUse/contentPusedoElemenmt.js
+++ js/6_CanUse/contentPusedoElemenmt.js
@@ -4,13 +4,14 @@ if( DEFINE_CONTENT_TEST_ID ){
             if( cssAvailability && !g_Type_notUndefined( g_CanUse_contentPseudoElement ) ){
                 var elm = DOM_createThenAdd(
                     g_body,
-                    'a', // 'span' だと Firefox72 で .offsetWidth が 0 で判定に失敗する
+                    'a',
                     {
                         'aria-hidden' : 'true',
                         id            : DEFINE_CONTENT_TEST_ID
                     }
                 );
-                g_CanUse_contentPseudoElement = !!elm.offsetWidth;
+                // Firefox 72.0, IE8~9, Edge で判定に失敗する為、無条件に良しとする…
+                g_CanUse_contentPseudoElement = g_Gecko || g_Goanna || 8 <= g_Trident || g_EdgeHTML || 6 <= elm.offsetWidth;
                 DOM_remove( elm );
             };
         }
