ee8b31cd213b30b3d7e3907f407a9aa1202241e7
itozyun <itozyun@gmail.com>
2020-02-07 15:01:24 +0900

    gulpfile.js was rewritten because ClosureCompiler was not working.

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,85 @@
-var gulp = require('gulp'),
-    minjs = '';
+const gulp = require('gulp'),
+      name = 'wdb';
+var   minjs = '';
 
 /* -------------------------------------------------------
  *  gulp js
  */
-gulp.task('createMinJs', function( cb ){
-    // https://github.com/google/closure-compiler-npm/tree/master/packages/google-closure-compiler
-    // https://stackoverflow.com/questions/56477837/google-closure-compiler-basic-process-example
-    const ClosureCompiler = require('google-closure-compiler').compiler,
-          globalVariables = 'document,navigator,screen,parseFloat,Number',
-          externs         = [ './inline-js/__externs.js' ];
+const closureCompiler = require('google-closure-compiler').gulp(),
+      globalVariables = 'document,navigator,screen,parseFloat,Number',
+      externs         = [ './inline-js/__externs.js' ],
+      tempDir         = require('os').tmpdir() + '/' + name;
 
-    new ClosureCompiler(
-        {
-            js                : [
-                './inline-js/01_ua.js',
-                './inline-js/02_uaPlatform.js',
-                './inline-js/03_uaEngine.js',
-                './inline-js/04_uaBrand.js',
-                './inline-js/05_uaFinish.js',
-                './inline-js/10_dynamicViewPort.js'
-            ],
-            externs           : externs,
-            compilation_level : 'ADVANCED',
-            // compilation_level : 'WHITESPACE_ONLY',
-            // formatting        : 'PRETTY_PRINT',
-            // warning_level     : 'VERBOSE',
-            language_in       : 'ECMASCRIPT3',
-            language_out      : 'ECMASCRIPT3',
-            output_wrapper    : 'ua={};(function(ua,window,' + globalVariables + ',undefined){\n%output%\n})(ua,this,' + globalVariables + ')'
-        }
-    ).run(
-        function(exitCode, stdOut, stdErr){
-            const writeFileSync = require('fs').writeFileSync,
-                  path = require('os').tmpdir() + '/compiled.js';
-
-            writeFileSync( path, stdOut, 'UTF-8' );
-            new ClosureCompiler(
+gulp.task('js', gulp.series(
+    function(){
+        return closureCompiler(
                 {
-                    js           : path,
-                    externs      : externs,
-                    language_in  : 'ECMASCRIPT3',
-                    language_out : 'ECMASCRIPT3'
+                    js                : [
+                        './inline-js/01_ua.js',
+                        './inline-js/02_uaPlatform.js',
+                        './inline-js/03_uaEngine.js',
+                        './inline-js/04_uaBrand.js',
+                        './inline-js/05_uaFinish.js',
+                        './inline-js/10_dynamicViewPort.js'
+                    ],
+                    externs           : externs,
+                    compilation_level : 'ADVANCED',
+                    //compilation_level : 'WHITESPACE_ONLY',
+                    //formatting        : 'PRETTY_PRINT',
+                    warning_level     : 'VERBOSE',
+                    language_in       : 'ECMASCRIPT3',
+                    language_out      : 'ECMASCRIPT3',
+                    output_wrapper    : 'ua={};(function(ua,window,' + globalVariables + ',undefined){\n%output%\n})(ua,this,' + globalVariables + ')',
+                    js_output_file    : 'temp.js'
                 }
-            ).run(
-                function(exitCode, _stdOut, stdErr){
-                    minjs = _stdOut;
-                    console.log(minjs.length + 'bytes.');
-                    cb();
-                }
-            );
-        }
-    );
+            )
+            .src()
+            .pipe(gulp.dest( tempDir ));
+    },
+    function(){
+        return closureCompiler(
+            {
+                js                : [
+                    tempDir + '/temp.js'
+                ],
+                externs           : externs,
+                // compilation_level : 'WHITESPACE_ONLY',
+                // formatting        : 'PRETTY_PRINT',
+                language_in       : 'ECMASCRIPT3',
+                language_out      : 'ECMASCRIPT3',
+                js_output_file    : 'temp2.js'
+            }
+        )
+        .src()
+        .pipe(gulp.dest( tempDir ));
+    },
+    function( cb ){
+        const readFileSync = require('fs').readFileSync;
+        
+        minjs = readFileSync( tempDir + '/temp2.js' ).toString();
+        console.log(minjs.length + 'bytes.');
+        cb();
+    },
+    function(){
+        const cheerio = require('gulp-cheerio');
+
+        return gulp.src('./ua.html')
+            .pipe(cheerio({
+                run: function ($) {
+                    $('script').eq(1).text( minjs );
+                } //, parserOptions: { xmlMode: true }
+            }))
+            .pipe(gulp.dest('./'));
+    }
+));
+
+/* -------------------------------------------------------
+ *  gulp html modifier
+ */
+const modifier = require('./gulp-html-modifier.js');
+
+gulp.task( 'mod', function(){
+    return gulp.src('./mod.2.html')
+        .pipe(modifier())
+        .pipe(gulp.dest('./mod'));
 });
-
-var cheerio = require('gulp-cheerio');
-
-gulp.task( 'insertMinJs', function(){
-    return gulp.src('./ua.html')
-        .pipe(cheerio({
-            run: function ($) {
-                $('script').eq(1).text( minjs );
-            } //, parserOptions: { xmlMode: true }
-        }))
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('js', gulp.series( 'createMinJs', 'insertMinJs' ) );
