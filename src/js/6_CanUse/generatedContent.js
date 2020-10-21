68be036215af70f3279c3b49ed650353e7c5172c
itozyun <itozyun@gmail.com>
2020-10-21 16:15:16 +0900

    Fixed : Failed to determine css-generated-content in Opera 10<=.

--- src/js/6_CanUse/generatedContent.js
+++ src/js/6_CanUse/generatedContent.js
@@ -11,7 +11,7 @@ if( WEB_DOC_BASE_DEFINE_CONTENT_TEST_ID ){
                     // Firefox 72.0, IE8~9, Edge, OSX Safari 13, iOS Safari 12 で判定に失敗する為、無条件に良しとする…
                     g_generatedContentEnabled = 1.8 <= g_Gecko || g_Goanna || 8 <= g_Trident || g_EdgeHTML ||
                                                     // https://developer.mozilla.org/ja/docs/Web/CSS/::after
-                                                    530 <= g_WebKit || 3.2 <= g_SafariMobile;
+                                                10 <= g_Presto || 530 <= g_WebKit || 3.2 <= g_SafariMobile;
                     if( !g_generatedContentEnabled ){
                         var elm = DOM_createThenAdd(
                             g_body,
