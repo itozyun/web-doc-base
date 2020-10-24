cde6d0fda5c83ff8657d3791cad735582380d3ec
itozyun <itozyun@gmail.com>
2020-10-25 03:15:07 +0900

    CSS generated content test:fix for Firefox3.5&3.6

--- src/js/6_CanUse/generatedContent.js
+++ src/js/6_CanUse/generatedContent.js
@@ -10,12 +10,11 @@ if( WEB_DOC_BASE_DEFINE_CONTENT_TEST_ID ){
                 if( !g_notUndefined( g_generatedContentEnabled ) ){
                     // Firefox 72.0, IE8~9, Edge, OSX Safari 13, iOS Safari 12 で判定に失敗する為、無条件に良しとする…
                     g_generatedContentEnabled = g_Goanna || 8 <= g_Trident || g_EdgeHTML ||
-                                                // Firefox 3.5 以上
-                                                2 <= g_Gecko || ( ( ua.ENGINE_VERSION === '1.9.1' || ua.ENGINE_VERSION === '1.9.2' ) && g_Gecko ) ||
-                                                    // https://developer.mozilla.org/ja/docs/Web/CSS/::after
-                                                // Presto Opera: CSS-P をサポートするのは 7.11 より後で 7.23 以下のどこか
-                                                // 7.11 では display, width, height には対応
-                                                7.2 <= g_Presto || 530 <= g_WebKit || 3.2 <= g_SafariMobile ? 2 :
+                                                // Firefox : CSS-P をサポートするのは Firefox 3.5 以降, Gecko 1.9.1 以降. Firefox 3.5.19 は Gecko 1.9.1.19
+                                                2 <= g_Gecko || ( g_Gecko && ( ua.ENGINE_VERSION.indexOf( '1.9.1' ) === 0 || ua.ENGINE_VERSION.indexOf( '1.9.2' ) === 0 ) ) ||
+                                                // Opera : CSS-P をサポートするのは 7.20 以降
+                                                7.2 <= g_Presto ||
+                                                530 <= g_WebKit || 3.2 <= g_SafariMobile ? 2 :
                                                 6   <= g_Presto || g_Gecko ? 1 : 0;
                     if( !g_generatedContentEnabled ){
                         var elm = DOM_createThenAdd(
