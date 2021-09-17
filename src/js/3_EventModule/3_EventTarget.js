99f1cd69de760a205aed92b554a2df9bb3fbe82b
itozyun <itozyun@gmail.com>
2021-09-18 06:57:33 +0900

    Fixed preventDefault in the old safari.

--- src/js/3_EventModule/3_EventTarget.js
+++ src/js/3_EventModule/3_EventTarget.js
@@ -12,8 +12,8 @@ p_removeEventListener = EventTraget_removeEventListener;
 /** @type {Object<string, Object>} */
 var EventTarget_LISTENERS        = {},
     EventTarget_USE_ATTACH       = false, // 5 <= p_Trident && p_Trident < 9,
-    EventTarget_USE_STANDERD     = !p_Tasman && window.addEventListener,
     EventTarget_PATCH_OLD_WEBKIT = p_WebKit < 525.13, // Safari <3
+    EventTarget_USE_STANDERD     = !EventTarget_PATCH_OLD_WEBKIT && !p_Tasman && window.addEventListener,
     EventTarget_safariPreventDefault;
 
 /** 1.
