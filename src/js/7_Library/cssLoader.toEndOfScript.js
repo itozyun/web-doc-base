179f7ea387c120791e0dd7329dbee9269985c875
itozyun <itozyun@gmail.com>
2021-09-18 21:44:19 +0900

    Refactoring : cssLoader.

--- src/js/7_Library/cssLoader.toEndOfScript.js
+++ src/js/7_Library/cssLoader.toEndOfScript.js
@@ -1,24 +1,26 @@
-
+if(
 /**
- * load modern.css or legacy.css
+ * IE9以下以外かつ Mac IE 以外に modern.css, legacy.css を読み込む
  */
-if( !( p_Trident < 10 ) && !p_Tasman ){
-    document.write( '<link href="' + p_assetUrl + p_cssName + '" rel="stylesheet" type="text\/css" media="screen,handheld,projection,tv,print">' );
-};
-
+    ( !( p_Trident < 10 ) && !p_Tasman )
+    ||
 /**
  * ie11 の ie5 モード で css が読み込まれない問題の対策
  */
-if( p_IEVersion === 11 && p_Trident === 5 ){
-    p_DOM_insertElement(
-        p_head,
-        'link',
-        {
-            href : p_assetUrl + p_cssName,
-            rel  : 'stylesheet',
-            type : 'text/css'
+    ( p_IEVersion === 11 && p_Trident === 5 )
+){
+    p_listenLoadEvent(
+        function(){
+            p_DOM_insertElement(
+                p_head,
+                'link',
+                {
+                    href  : p_assetUrl + p_cssName,
+                    rel   : 'stylesheet',
+                    type  : 'text/css',
+                    media : 'screen,handheld,projection,tv,print'
+                }
+            );
         }
     );
-    // document.createStyleSheet( p_assetUrl + p_cssName );
-    // p_head.appendChild( document.createElement( '<link href="' + p_assetUrl + p_cssName + '" rel=stylesheet type="text/css">' ) );
 };
