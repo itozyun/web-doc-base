bcdbeeb1098b82df3c0c66455047ac34e604f2ed
itozyun <itozyun@gmail.com>
2021-10-06 03:43:15 +0900

    Added a warning when DEFINE_WEB_DOC_BASE__LOGGER_ELEMENT_ID is empty.

--- src/js/2_CoreModule/DebugLogger.toEndOfScript.js
+++ src/js/2_CoreModule/DebugLogger.toEndOfScript.js
@@ -1,5 +1,6 @@
 if( DEFINE_WEB_DOC_BASE__DEBUG ){
     p_listenLoadEvent(function(){
+       
         var DebugLogger_elm;
 
         if( DEFINE_WEB_DOC_BASE__LOGGER_ELEMENT_ID ){
@@ -7,17 +8,23 @@ if( DEFINE_WEB_DOC_BASE__DEBUG ){
             if( !DebugLogger_elm ){
                 alert( '#' + DEFINE_WEB_DOC_BASE__LOGGER_ELEMENT_ID + ' not found!' );
             };
+        } else {
+            alert( 'No DEFINE_WEB_DOC_BASE__LOGGER_ELEMENT_ID !' );
         };
+
         if( DebugLogger_elm ){
             Debug.log = function( text ){
                 p_DOM_insertElement( DebugLogger_elm, 'P', null, text );
             };
         } else if( window.console ){
             Debug.log = console.log;
+        } else {
+            Debug.log = p_emptyFunction;
         };
 
         while( _p_Debug_logsBeforeLoad.length ){
             Debug.log( _p_Debug_logsBeforeLoad.shift() );
         };
+        _p_Debug_logsBeforeLoad = null;
     });
 };
