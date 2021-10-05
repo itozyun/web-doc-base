75f510c9ed98a38dab94d53bbc02e7ea3403c3ff
itozyun <itozyun@gmail.com>
2021-10-06 03:47:24 +0900

    Support for Opera 8 and Gecko 0.9.4 to the cssLoader.

--- src/js/7_Library/cssLoader.toEndOfScript.js
+++ src/js/7_Library/cssLoader.toEndOfScript.js
@@ -9,18 +9,24 @@ if(
  */
     ( p_IEVersion === 11 && p_Trident === 5 )
 ){
-    p_listenLoadEvent(
-        function(){
-            p_DOM_insertElement(
-                p_head,
-                'link',
-                {
-                    href  : p_assetUrl + p_cssName,
-                    rel   : 'stylesheet',
-                    type  : 'text/css',
-                    media : 'screen,handheld,projection,tv,print'
-                }
-            );
-        }
-    );
+    if( p_Presto < 9 || // document.write でないと、コンテンツ量によって CSS が一部しか適用されない不具合に遭遇する
+        p_Gecko < 1     // Gecko 0.9.4 以下で確認 0.9.6 では発生しない document.write でないと css が適用されない
+    ){
+        document.write('<link href="' + p_assetUrl + p_cssName + '" rel="stylesheet" type="text/css" media="screen,handheld,print">');
+    } else {
+        p_listenLoadEvent(
+            function(){
+                p_DOM_insertElement(
+                    p_head,
+                    'link',
+                    {
+                        href  : p_assetUrl + p_cssName,
+                        rel   : 'stylesheet',
+                        type  : 'text/css',
+                        media : 'screen,handheld,projection,tv,print'
+                    }
+                );
+            }
+        );
+    };
 };
