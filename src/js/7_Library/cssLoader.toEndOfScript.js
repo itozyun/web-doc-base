aed6925cadc57ac0e614082e6e771b858a7d6e7e
itozyun <itozyun@gmail.com>
2022-02-09 05:04:53 +0900

    Fix the cssLoader.

--- src/js/7_Library/cssLoader.toEndOfScript.js
+++ src/js/7_Library/cssLoader.toEndOfScript.js
@@ -10,14 +10,16 @@ if(
  */
     ( p_IEVersion === 11 && p_Trident === 5 )
 ){
-    if( p_Presto < 9.5 || // Presto 9.27 noscript 下のコンテンツが取れない, document.write でないと、コンテンツ量によって CSS が一部しか適用されない不具合に遭遇する
-        p_Gecko  < 1.5 || // Gecko 1.4.1 noscript 下のコンテンツが取れない, Gecko 0.9.4 以下で確認 0.9.6 では発生しない document.write でないと css が適用されない
+    if( p_Presto < 9 || // document.write でないと、コンテンツ量によって CSS が一部しか適用されない不具合に遭遇する
+        p_Gecko  < 1 || // Gecko 0.9.4 以下で確認 0.9.6 では発生しない document.write でないと css が適用されない
         !p_Tasman && !window.addEventListener // Tasman と Trident 以外で addEventListener をサポートしないブラウザ(未確認)
     ){
         document.write('<link href="' + p_assetUrl + p_cssDir + p_screenModeDir + p_cssName + '" rel="stylesheet" type="text/css" media="screen,handheld,print">');
     } else if(
-        p_WebKit <= 532 || // Windows XP + Safari 4.0.5-, noscript.textContent が ""
-        p_ChromiumBase < 2 || // Windows XP + Chrome, noscript.textContent が ""
+        p_Presto   <  9.5 || // Windows XP + Presto 9.27   , noscript 下のコンテンツが取れない
+        p_Gecko    <  1.5 || // Windows XP + Gecko  1.4.1  , noscript 下のコンテンツが取れない
+        p_WebKit <= 532 || // Windows XP + Safari 4.0.5- , noscript.textContent が ""
+        p_ChromiumBase <  2 || // Windows XP + Chrome 1.0.154, noscript.textContent が ""
         !DEFINE_WEB_DOC_BASE__USE_CSS_LOADER_OF_INLINE_CSS
     ){
         p_listenLoadEvent(
