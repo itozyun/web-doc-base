c0a4adcb6480f7ef18065ea1038a37657175f962
itozyun <itozyun@gmail.com>
2022-03-06 23:48:06 +0900

    nodeCleaner.js : Optimized.

--- src/js/3_DOM/nodeCleaner.js
+++ src/js/3_DOM/nodeCleaner.js
@@ -5,7 +5,9 @@ p_loadEventCallbacks.splice( 0, 0, // onload の一番最初に追加
         var removeCommentNodes  = !( p_Presto < 8 || p_Trident < 5 || p_Tasman || p_Gecko < 0.9 ),
             hasDynamicStyleBug  = p_Trident < 7,
             removalMetaTagNames = [ 'og:', 'twitter:', 'fb:' ],
-            moveToHead          = [];
+            nodesToGoUnderHead  = [],
+            nodesBeRemoved      = [],
+            unknownTags         = {};
 
         walkTree( p_html );
 
@@ -18,22 +20,26 @@ p_loadEventCallbacks.splice( 0, 0, // onload の一番最初に追加
                 kid  = kids[ --i ]; // childNodes[ -1 ] は Firefox 3.5 でエラー
                 type = kid.nodeType;
                 if( type === 8 ){
-                    removeCommentNodes && p_DOM_remove( kid );
+                    removeCommentNodes && nodesBeRemoved.push( kid );
                 } else if( type === 1 ){
                     tag = p_DOM_getTagName( kid );
+                    if( tag.charAt( 0 ) === '/' ){
+                        tag = tag.substr( 1 );
+                        unknownTags[ tag ] = true; // 頭に / の付くタグを控える
+                    };
 
-                    switch( tag.replace( '/', '' ) ){
+                    switch( tag ){
                         case 'STYLE' :
                             if( 8 <= p_Presto && p_Presto < 9 ) break; /* Opera 8 は <style> の移動が反映されない */
                         case 'LINK' :
                             !m_isIE4DOM && !hasDynamicStyleBug && /* IE6 で link を移動すると style の適用がガタガタ */
-                                !p_DOM_contains( p_head, kid ) && moveToHead.push( kid );
+                                !p_DOM_contains( p_head, kid ) && nodesToGoUnderHead.push( kid );
                             break;
                         case 'META' :
                             name = p_DOM_getAttribute( kid, 'name' ) || p_DOM_getAttribute( kid, 'property' );
                             for( j = removalMetaTagNames.length; j; ){
                                 if( name.indexOf( removalMetaTagNames[ --j ] ) === 0 ){
-                                    p_DOM_remove( kid );
+                                    nodesBeRemoved.push( kid );
                                     break;
                                 };
                             };
@@ -43,33 +49,27 @@ p_loadEventCallbacks.splice( 0, 0, // onload の一番最初に追加
                         case 'NOSCRIPT' : //'NOFRAMES' //'NOEMBED' //'NOLAYER'
                             if( p_DOM_hasAttribute( kid, 'skip-cleanup' ) ) break;
                         case '!' :
-                            p_DOM_remove( kid );
+                            nodesBeRemoved.push( kid );
                             break;
                         case 'SOURCE' :
                             if( !window.HTMLSourceElement ){
-                                p_DOM_remove( kid );
-                            };
-                            break;
-                        case 'HEADER' :
-                        case 'MAIN' :
-                        case 'NAV' :
-                        case 'ARTICLE' :
-                        case 'SECTION' :
-                        case 'ASIDE' :
-                        case 'FIGURE' :
-                        case 'FIGCAPTION' :
-                        case 'FOOTER' :
-                            if( p_Trident < 9 ){
-                                p_DOM_remove( kid );
+                                nodesBeRemoved.push( kid );
                             };
                             break;
                         default :
-                            p_DOM_getChildNodes( kid ).length && walkTree( kid );                        
+                            if( unknownTags[ tag ] ){
+                                nodesBeRemoved.push( kid );
+                            } else {
+                                p_DOM_getChildNodes( kid ).length && walkTree( kid );
+                            };
                     };
                 };
             };
-            while( moveToHead[ 0 ] ){
-                p_head.appendChild( moveToHead.pop() );
+            while( nodesToGoUnderHead[ 0 ] ){
+                p_head.appendChild( nodesToGoUnderHead.pop() );
+            };
+            while( nodesBeRemoved[ 0 ] ){
+                p_DOM_remove( nodesBeRemoved.pop() );
             };
         };
     }
