6ff137fe35ce8e48a4e7d4c20940b1acd7ca249b
itozyun <itozyun@gmail.com>
2022-03-18 01:27:29 +0900

    Change default colors.

--- gulpfile.js
+++ gulpfile.js
@@ -295,9 +295,9 @@ const plumber     = require("gulp-plumber"),
 gulp.task('css', function(){
     return gulp.src([
             "./src/scss/01_Variables/01_BuildTargets.scss",
-            "./src/scss.docs/docs_color.scss",
+            // "./src/scss.docs/docs_color.scss",
             "./src/scss/**/*.scss",
-            "./src/scss.docs/**.scss"
+            // "./src/scss.docs/**.scss"
         ])
         .pipe(plumber())
         .pipe(
