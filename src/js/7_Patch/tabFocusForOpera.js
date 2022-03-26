4ecba76df50af5ce4a36651bdf171f434a0f880b
itozyun <itozyun@gmail.com>
2022-03-27 04:59:50 +0900

    Lower limit version of Opera's tab focus fixed.

--- src/js/7_Patch/tabFocusForOpera.js
+++ src/js/7_Patch/tabFocusForOpera.js
@@ -1,6 +1,5 @@
 /**
- * Opera 8~12 はタブフォーカスの挙動が異なる為、tabindex を動的に書き換えて対応する。
- * Opera 7.x はタブフォーカスをサポートしない。
+ * Opera 7~12 はタブフォーカスの挙動が異なる為、tabindex を動的に書き換えて対応する。
  *
  * 2011年7月現在で、Internet Explorer と Opera は、デフォルトではフォーカス可能でない要素に関して、負の整数を指定した tabindex コンテンツ属性に対応 
  * https://books.google.co.jp/books?id=ZSsfUNa2nMgC&pg=PA562&lpg=PA562&dq=Opera+tabindex&source=bl&ots=TbzzkHtakR&sig=ACfU3U1fIWCpEwgJrRBLfY6DPdvLFwK12w&hl=ja&sa=X&ved=2ahUKEwjWxO6EsNrzAhXIMN4KHazmBzkQ6AF6BAgXEAM#v=onepage&q=Opera%20tabindex&f=false
@@ -10,19 +9,18 @@
  */
 p_Presto && p_listenLoadEvent(
     function(){
-        if( 8 <= p_Presto ){
-            var anchorList = p_DOM_getElementsByTagNameFromDocument( 'a' ),
+        var anchorList = p_DOM_getElementsByTagNameFromDocument( 'a' ),
             l = anchorList.length, i = 0, elm;
 
-            for( ; i < l ; ++i ){
-                elm = anchorList[ i ];
-                if( p_DOM_getAttribute( elm, 'tabindex' ) === '-1' ){
-                    p_DOM_removeAttribute( elm, 'tabindex' );
-                } else if( p_DOM_getAttribute( elm, 'href' ) ){
-                    p_DOM_setAttribute( elm, 'tabindex', '0' );
-                };
+        for( ; i < l ; ++i ){
+            elm = anchorList[ i ];
+            if( p_DOM_getAttribute( elm, 'tabindex' ) === '-1' ){
+                p_DOM_removeAttribute( elm, 'tabindex' );
+            } else if( p_DOM_getAttribute( elm, 'href' ) ){
+                p_DOM_setAttribute( elm, 'tabindex', '0' );
             };
         };
+
         var inputList = p_DOM_getElementsByTagNameFromDocument( 'input' );
 
         for( i = 0, l = inputList.length; i < l ; ++i ){
