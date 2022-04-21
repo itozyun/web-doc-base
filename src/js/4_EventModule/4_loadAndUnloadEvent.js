b9ae42bf821561b205a7c650933371e158397086
itozyun <itozyun@gmail.com>
2022-04-21 12:02:55 +0900

    Workaround for browser crashes under Gecko 0.8.1.

--- src/js/4_EventModule/4_loadAndUnloadEvent.js
+++ src/js/4_EventModule/4_loadAndUnloadEvent.js
@@ -58,7 +58,8 @@ if( p_WebKit <= 419.3 ){ // Safari 2-
 // https://web.archive.org/web/20180328040501/http://oogatta.hatenadiary.jp/entry/20121228/1356696182
 //   bfcache について覚えて帰ってもらいます。（転載）
 // IE と Firefox <1.5(Gecko <1.8) だけ unload でイベントリスナを削除する
-if( p_Trident || p_Gecko < 1.8 ){
+// Gecko 0.8.1 以下は unload でブラウザがクラッシュする https://twitter.com/itozyun/status/1516964450083160064
+if( p_Trident || 0.9 <= p_Gecko && p_Gecko < 1.8 ){
     p_addEventListener( window, 'unload', function( e ){
         m_dispatchEvent( /** @type {!Array.<!Function>} */ (Event_unloadEventCallbacks), e, true );
     } );
