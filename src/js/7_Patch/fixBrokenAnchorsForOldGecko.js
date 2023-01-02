22a4bf5ce8568f19bfa8bd8b94e4cd4f600078c7
itozyun <itozyun@gmail.com>
2023-01-03 03:10:43 +0900

    Added fixBrokenAnchorsForOldGecko.js

---
+++ src/js/7_Patch/fixBrokenAnchorsForOldGecko.js
@@ -0,0 +1,58 @@
+/**
+ * アーリー Gecko で a タグ周りのタグ構造が乱れるのを DHTML で修復する
+ *   1. RichLink で空の <a href> と ブロック要素の下に <a href> ができる
+ *   2. スキーマ(schema.org)を含む <a href> の直前に、空の <a href> ができる、これは不可視の為、フォーカスが消える問題を起こす
+ */
+if( p_Gecko && ua.conpare( p_engineVersion, '0.9.0' ) < 0 ){
+    p_loadEventCallbacks.splice( 1, 0, // noadCleaner の次に追加
+        // 1.
+        function(){
+            var richLinkList = p_DOM_getElementsByClassName( p_elmMain, 'RichLink-sep' ),
+                richLink, kids, root, kid, anchorList, anchor, anchorKids;
+
+            while( richLink = richLinkList.shift() ){
+                kids = p_DOM_getChildNodes( richLink );
+                while( root = kids.shift() ){ // A.
+                    if( p_DOM_getTagName( root ) === 'A' && !root.innerHTML ){
+                        Debug.log( '■ Broken Anchor ' + root.parentNode.innerHTML );
+                        while( kid = kids.shift() ){
+                            if( kid.nodeType === 1 ){
+                                if( p_DOM_getTagName( kid ) === 'A' ){
+                                    // alert( root.innerHTML );
+                                    break; // => A.
+                                };
+                                anchorList = p_DOM_getElementsByTagName( kid, 'A' );
+                                while( anchor = anchorList.shift() ){
+                                    // alert( anchor.innerHTML );
+                                    anchorKids = p_DOM_getChildNodes( anchor );
+                                    while( anchorKids.length ){
+                                        anchor.parentNode.insertBefore( anchorKids.shift(), anchor );
+                                    };
+                                    p_DOM_remove( anchor );
+                                };
+                            };
+                            root.appendChild( kid );
+                        };
+                    };
+                };
+            };
+        },
+        // 2.
+        function(){
+            var anchorList = p_DOM_getElementsByTagNameFromDocument( 'A' ),
+                anchor, originalAnchor;
+
+            while( anchor = anchorList.shift() ){
+                if( p_DOM_getTagName( anchor ) === 'A' && !anchor.innerHTML ){
+                    originalAnchor = anchor.nextSibling && anchor.nextSibling.firstChild;
+                    if( originalAnchor ){
+                        if( originalAnchor.href === anchor.href ){
+                            Debug.log( '▲ Broken Anchor ' + originalAnchor.innerHTML );
+                            p_DOM_remove( anchor );
+                        };
+                    };
+                };
+            };
+        }
+    );
+};
