1325cb9f469ce0a48386473523fa7723176278ff
itozyun <itozyun@gmail.com>
2023-02-20 13:49:43 +0900

    img[loading=lazy] Minor fix.

--- src/js/7_Patch/detectImageTurnedOff.js
+++ src/js/7_Patch/detectImageTurnedOff.js
@@ -9,7 +9,7 @@ p_listenImageReady(
         // /img-loaded を親に追加する、但し aRootBody は除く
         if( !directlyUnder && imgReady ){
             p_DOM_addClassName( parent, DEFINE_WEB_DOC_BASE__CLASSNAME_IMG_LOADED );
-        } else if( !imgReady && !p_DOM_getAttribute( imgElement, 'alt' ) ){
+        } else if( !imgReady && !p_DOM_getAttribute( imgElement, 'alt' ) && p_DOM_getAttribute( imgElement, 'loading' ) !== 'lazy' ){
         // alt の無い画像を非表示に
             p_DOM_setStyle( imgElement, 'display', 'none' );
         };
